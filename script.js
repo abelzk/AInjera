5fe000bae978527846bdf46aa128b514efc20dce
abelzeki24@gmail.com <abelzeki24@gmail.com>
2023-05-10 17:34:56 -0800

    chatbot update

--- script.js
+++ script.js
@@ -1,39 +1,73 @@
+
+
+// The text to be typed
+var text = "Hi there, I’m AInjera, the AI chatbot that knows everything about Ethiopian cuisine and culture. I’m happy to share my knowledge with you and answer any questions you might have. Whether you want to know how to prepare injera, the staple flatbread, or what are some of the most popular dishes and spices in Ethiopia, I’m here to help. I can also tell you about the history, geography, and traditions of this amazing country. Just type your question and I’ll try to give you a satisfying answer. Let’s have some fun! 😊";
+
+// The element where the text will be displayed
 var typewriter = document.getElementById("typewriter");
 
-// Define the text to be typed out
-var text = "Hi there, I’m AInjera, the AI chatbot that knows everything about Ethiopian cuisine and culture. I’m happy to share my knowledge with you and answer any questions you might have. Whether you want to know how to prepare injera, the staple flatbread, or what are some of the most popular dishes and spices in Ethiopia, I’m here to help. I can also tell you about the history, geography, and traditions of this amazing country. Just type your question and I’ll try to give you a satisfying answer. Let’s have some fun!";
-
-// Define the typing speed in milliseconds
-var speed = 30;
-
-// Define a variable to store the current index of the text
+// The index of the current character
 var index = 0;
 
-// Define a function to type out one character at a time
+// The speed of typing in milliseconds
+var speed = 30;
+
+// A counter to keep track of how many conversations have been started
+var count = 0;
+
+// A flag to indicate if the chatbot should ask more questions
+var askMore = true;
+
+// The function that types one character at a time
 function type() {
   // If the index is less than the length of the text
   if (index < text.length) {
-    // Append the next character to the typewriter element
-   var char = text[index];
-// add the character to the end of the innerHTML
-typewriter.innerHTML = typewriter.innerHTML + char;
-    // Increment the index by one
+    // Append the current character to the typewriter element
+    typewriter.innerHTML += text.charAt(index);
+    // Increment the index
     index++;
     // Call the type function again after a delay
     setTimeout(type, speed);
   } else {
-    // If the index is equal to the length of the text, start blinking the cursor
-    blink();
+    // If the index is equal to the length of the text
+    // If the chatbot should ask more questions and the counter is less than 3
+    if (askMore && count < 3) {
+      // Wait for 3 seconds and then start a new conversation
+      setTimeout(newConversation, 3000);
+    }
   }
-} 
-
-// Define a function to blink the cursor periodically
-function blink() {
-  // Toggle the visibility of the border-right property
-  typewriter.style.borderRight = typewriter.style.borderRight ? "" : "0.15em solid #00be894";
-  // Call the blink function again after a delay
-  setTimeout(blink, 500);
 }
 
-// Call the type function to start the typewriter effect
-type();
+// The function that starts a new conversation
+function newConversation() {
+  // Reset the index
+  index = 0;
+  // Increment the counter
+  count++;
+  // Change the text to a new one based on the counter
+  switch (count) {
+    case 1:
+      text =  "What are some of your hobbies?";
+      break;
+    case 2:
+      text = "What kind of music do you like?";
+      break;
+    case 3:
+      text = "Where do you live?";
+      // Set the flag to false so that no more questions are asked
+      askMore = false;
+      break;
+    default:
+      text = "";
+      break;
+  }
+  
+  // Add a line break to the typewriter element
+  typewriter.innerHTML += "<br>";
+  
+  // Call the type function again
+  type();
+}
+
+// Call the type function when the page loads
+window.onload = type;
